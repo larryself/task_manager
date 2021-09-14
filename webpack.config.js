49ea5d725af93de64b1f434f2b4957783d45c145
Yan <yanmusikhin@gmail.com>
2021-09-14 13:50:35 +0400

    init: load project

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const PrettierPlugin = require('prettier-webpack-plugin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const CopyPlugin = require('copy-webpack-plugin');
+const filename = (ext) => `[name].[contenthash].${ext}`;
+
+const lintPlugin = (isDev) => isDev ? [] : [new ESLintPlugin({ extensions: ['js'] }),
+  new StyleLintPlugin({ extensions: ['css', 'scss'] }),
+  new PrettierPlugin(),];
+
+module.exports = ({ develop }) => ({
+  mode: develop ? 'development' :  'production',
+  devtool: develop ? 'inline-source-map' : false,
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `./js/${filename('js')}`,
+    publicPath: '',
+    assetModuleFilename: '[path][name][ext]',
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.html$/i,
+      //   loader: 'html-loader',
+      // },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(?:gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(|woff|eot|ttf)$/,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+      minify: {
+        collapseWhitespace: true,
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `${filename('css')}`
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from:'./assets', to : 'assets'}
+      ],
+    }),
+    new ImageminPlugin({
+      plugins: [
+        imageminMozjpeg({
+          quality: 90,
+          progressive: true
+        })
+      ]
+    }),
+    ...lintPlugin(develop)
+  ]
+});
